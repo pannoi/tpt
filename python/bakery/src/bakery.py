fce1f90a8dd2741685903265552bf363103f5c36
Anton Poplavnoi <48126600+pannoi@users.noreply.github.com>
2020-09-08 20:01:41 +0300

    Create bakery.py

---
+++ python/bakery/src/bakery.py
@@ -0,0 +1,34 @@
+class Bakery:
+    """Class bakery."""
+
+    def __init__(self, name: str, min_experience_level: int, budget: int):
+        """Class constructor."""
+        pass
+
+    def add_baker(self, baker: Baker) -> Baker:
+        """Add baker."""
+        pass
+
+    def remove_baker(self, baker: Baker):
+        """Remove baker."""
+        pass
+
+    def add_recipe(self, name: str):
+        """Add recipe."""
+        pass
+
+    def make_order(self, name: str) -> Pastry:
+        """Make order."""
+        pass
+
+    def get_recipes(self) -> dict:
+        """Get recipes."""
+        pass
+
+    def get_pastries(self) -> list:
+        """Get pastries."""
+        pass
+
+    def get_bakers(self) -> list:
+        """Get baker."""
+        pass
